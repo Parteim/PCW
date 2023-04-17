7503d9bf95193e8655e808811c2747d59a961df7
Parteim <Sergeykaynov@gmail.com>
2023-04-17 23:24:58 +0700

    extends functional of bot

---
+++ PCW/VK/Bot/Domain/ImgFilter.py
@@ -0,0 +1,12 @@
+
+def img_filter(img_list):
+    filtered_img_list = []
+
+    for post in img_list:
+        for attachment in post['attachments']:
+            if attachment['type'] == 'photo':
+                for img in attachment['photo']['sizes']:
+                    if img['type'] == 'z':
+                        filtered_img_list.append(img['url'])
+
+    return filtered_img_list
--- PCW/VK/Bot/VkBotMethods/Wall.py
+++ PCW/VK/Bot/VkBotMethods/Wall.py
@@ -2,21 +2,39 @@ from PCW.VK.Bot.VkBot import VkBot
 
 
 class Wall(VkBot):
-
     def get_posts(self, group, count_of_posts=100):
         posts = []
         offset = 0
         count = 100
+        if count_of_posts <= 100:
+            response = super().get(
+                'wall.get', domain=group,
+                count=count,
+                offset=offset,
+            )
+            posts.extend(response.json()['response']['items'])
+        else:
+            while count_of_posts > 0:
+                response = super().get(
+                    'wall.get',
+                    domain=group,
+                    count=count,
+                    offset=offset,
 
-        response = super().get(
-            'wall.get',
-            domain=group,
-            count=count,
-            offset=offset,
+                )
+                posts.extend(response.json()['response']['items'])
 
-        )
-        posts.extend(response.json()['response']['items'])
-        print(posts)
+                count_of_posts -= 100
+
+                if count_of_posts > 100:
+                    offset += 100
+                else:
+                    count = count_of_posts
+                    offset += count_of_posts
+
+        return posts
 
 
-Wall().get_posts('fujiwaraclan')
+print(Wall().get_posts('fujiwaraclan'))
+
+
