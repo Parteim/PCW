837d4a3ba29ab04c63754a085a140237f8d3aba1
Parteim <48876969+Parteim@users.noreply.github.com>
2023-06-09 22:58:08 +0700

    Update ImgFilter.py

--- PCW/VK/Bot/Domain/ImgFilter.py
+++ PCW/VK/Bot/Domain/ImgFilter.py
@@ -23,10 +23,4 @@ def posts_img_filter(posts_list):
                         filtered_posts_list[-1].append(img['url'])
     return filtered_posts_list
 
-# bot = VkBotInstance(
-#     access_token='4f2c35104f2c35104f2c3510114c3c892044f2c4f2c35102c0950b4e8b4d099257fd1f0'
-# )
-#
-# posts = Wall(bot).get_posts('fujiwaraclan')
-#
-# print(posts_img_filter(posts))
+
